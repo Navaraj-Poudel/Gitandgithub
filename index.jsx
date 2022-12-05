99ec2e339b9aa34f385374198a458efef51fb2f9
Ajay Chaudhary <iamazoy110125@gmail.com>
2022-12-05 20:13:08 +0545

    learning git branch and commit

---
+++ index.jsx
@@ -0,0 +1,14 @@
+
+
+/*learning git branch and commit */
+
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Hello i am ajay and i am learning git and github
+    </div>
+  )
+}
+
+export default Index;
